c6c18cb4c752eb99d9e4b9cacdf952f7f6bcd8b3
rodgers <rodgerstarrodgers@gmail.com>
2025-02-01 00:14:40 +0300

    newsletterbox section build

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero.jsx";
 import LatestCollection from "../components/LatestCollection.jsx";
 import BestSeller from "../components/BestSeller.jsx";
 import OurPolicy from "../components/OurPolicy.jsx";
+import NewsLetterBox from "../components/NewsLetterBox.jsx";
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
             <LatestCollection/>
             <BestSeller/>
             <OurPolicy/>
+            <NewsLetterBox/>
         </div>
     )
 }
