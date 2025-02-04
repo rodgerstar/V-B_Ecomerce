19192674a412ac5efb86b05be6ad81ab110cfb18
rodgers <rodgerstarrodgers@gmail.com>
2025-02-05 02:58:42 +0300

    searchbar component build

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import {assets} from "../assets/assets.js";
 import {Link, NavLink} from "react-router-dom";
+import {ShopContext} from "../context/ShopContext.jsx";
 
 const Navbar = () => {
 
     const [visible,setVisible] = useState(false);
+
+    const {setShowSearch} = useContext(ShopContext)
 
     return(
         <div className= 'flex items-center justify-between py-5 font-medium'>
@@ -41,6 +44,7 @@ const Navbar = () => {
             <div
             className='flex items-center gap-6'>
                 <img
+                    onClick={()=>setShowSearch(true)}
                     className='w-5 cursor-pointer'
                     src={assets.search_icon} alt=""/>
 
