a997d12aecb4831aa5ab573e7d1dd1bb6e88537b
rodgers <rodgerstarrodgers@gmail.com>
2025-02-06 03:58:14 +0300

    cart section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,14 @@ import Orders from "./pages/Orders.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import SearchBar from "./components/SearchBar.jsx";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
     return (
         <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
 
+            <ToastContainer/>
             <Navbar/>
             <SearchBar/>
 
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -7,7 +7,7 @@ import RelatedProducts from "../components/RelatedProducts.jsx";
 const Product = () => {
 
     const {productId} = useParams()
-    const {products, currency} = useContext(ShopContext)
+    const {products, currency, addToCart} = useContext(ShopContext)
     const [productData, setProductData] = useState(false)
     const [image, setImage] = useState('')
     const [size, setSize] = useState('')
@@ -72,7 +72,7 @@ const Product = () => {
                             ))}
                         </div>
                     </div>
-                    <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+                    <button  onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
                     <hr className='mt-5 sm:w-4/5'/>
                     <div className='text-sm text-gray-500 mt-3 flex flex-col gap-1'>
                         <p>100% Original product</p>
