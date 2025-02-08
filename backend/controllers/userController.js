efb8437f1d5ea67ca1f5af74a2b39272726046b6
rodgers <rodgerstarrodgers@gmail.com>
2025-02-08 07:15:54 +0300

    Backend, creating API

---
+++ backend/controllers/userController.js
@@ -0,0 +1,24 @@
+
+
+
+
+// Route for user login
+const loginUser = async () => {
+
+}
+
+// Route for user registration
+const registerUser = async  (req,res) => {
+
+    res.json({msg:"Register Api Working"})
+
+}
+
+// route for admin login
+const adminLogin = async (req,res) => {
+
+}
+
+
+
+export {loginUser,registerUser,adminLogin}
