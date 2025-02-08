c20555cbd4037dd017d21fb4429285fa923a02ab
rodgers <rodgerstarrodgers@gmail.com>
2025-02-08 08:58:43 +0300

    Reinstalled dependencies and build register user APIS

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,12 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 
+const createToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
 
 
 // Route for user login
@@ -9,8 +16,45 @@ const loginUser = async () => {
 
 // Route for user registration
 const registerUser = async  (req,res) => {
+    try {
 
-    res.json({msg:"Register Api Working"})
+        const {name, email, password} = req.body;
+
+        // checking user exists
+        const  exists = await userModel.findOne({email})
+        if (exists) {
+            return res.json({success:false, message:"User already exists!"})
+        }
+
+        // validate email and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message:"Enter Valid Email!"})
+        }
+        if (password.length < 8) {
+            return res.json({success:false, message:"Enter a Strong Password!"})
+        }
+
+
+        // hash user passwords
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+
+        const token = createToken(user._id)
+
+        res.json ({success:true,token})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
 
 }
 
