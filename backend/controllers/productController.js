690cee086529991c8cd91f14f31500f010af4c04
rodgers <rodgerstarrodgers@gmail.com>
2025-02-09 13:39:01 +0300

    get single product API build

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -71,6 +71,17 @@ const removeProduct = async (req, res) => {
 };
 
 // function for single product info
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+    try {
+
+        const {productId} = req.body
+        const product = await productModel.findById(productId)
+        res.json({success:true,product})
+    }catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+};
+
 
 export { listProduct, addProduct, removeProduct, singleProduct };
