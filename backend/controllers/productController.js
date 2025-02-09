8cebf7c9d5ffa270ae9f907b181ad093ecb08865
rodgers <rodgerstarrodgers@gmail.com>
2025-02-09 13:29:47 +0300

    delete API build

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -59,7 +59,16 @@ const listProduct = async (req, res) => {
 };
 
 // function for remove product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+    try {
+
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true, message:"Products Removed!"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+};
 
 // function for single product info
 const singleProduct = async (req, res) => {};
