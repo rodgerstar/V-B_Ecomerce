318bfeb4c23b6eedde51c09c1c5df8a2b4768cbb
rodgers <rodgerstarrodgers@gmail.com>
2025-02-10 07:16:29 +0300

    make add product fields empty after adding goods

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {assets} from "../assets/assets.js";
 import axios from "axios";
+import {toast} from "react-toastify";
 
 const Add = ({token}) => {
 
@@ -40,10 +41,21 @@ const Add = ({token}) => {
 
             const response = await axios.post( "http://localhost:4000" + "/api/product/add", formData, {headers:{token}})
 
-            console.log(response.data)
-
+            if (response.data.success) {
+               toast.success(response.data.message)
+                setName('')
+                setDescription('')
+                setImage1(false)
+                setImage2(false)
+                setImage3(false)
+                setImage4(false)
+                setPrice('')
+            } else {
+                toast.error(response.data.message)
+            }
         }catch (error) {
-
+            console.log(error);
+            toast.error(error.message)
         }
     }
 
