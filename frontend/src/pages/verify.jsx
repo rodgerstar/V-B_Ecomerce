d6d01bf329819cad02351e27671776d5da7804ed
rodgers <rodgerstarrodgers@gmail.com>
2025-02-11 23:59:44 +0300

    verify page working

--- frontend/src/pages/verify.jsx
+++ frontend/src/pages/verify.jsx
@@ -1,17 +1,38 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {ShopContext} from "../context/ShopContext.jsx";
 import {useSearchParams} from "react-router-dom";
+import axios from "axios";
+import {toast} from "react-toastify";
 
 const Verify = () => {
      const {navigate, token, setCartItems} = useContext(ShopContext)
      const [serchParams, setSearchParams] = useSearchParams()
 
     const success = serchParams.get('success')
-    const prderId = setSearchParams.get('orderId')
+    const orderId = serchParams.get('orderId')
 
     const verifyPayment = async () => {
+         try {
+             if (!token) {
+                 return null
+             }
+             const respoonse = await axios.post("http://localhost:4000" + '/api/order/verifyStripe', {success,orderId}, {headers: {token}})
 
+             if (respoonse.data.success) {
+                 setCartItems({})
+                 navigate('/orders')
+             }else {
+                 navigate('/cart')
+             }
+         } catch (error) {
+             console.log(error)
+             toast.error(error.message)
+         }
     }
+
+    useEffect(() => {
+        verifyPayment()
+    }, [token]);
 
     return (
         <div>
