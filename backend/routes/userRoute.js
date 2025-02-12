680bd4c8916de9ddacf70ab7c1a0cde547535026
rodgers <rodgerstarrodgers@gmail.com>
2025-02-12 16:50:55 +0300

    vercel backend url

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -8,6 +8,9 @@ const userRouter = express.Router();
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 userRouter.post('/admin',adminLogin)
+userRouter.get('/test', (req, res) => {
+    res.json({ success: true, message: "User routes are working!" });
+});
 
 
 export default userRouter
